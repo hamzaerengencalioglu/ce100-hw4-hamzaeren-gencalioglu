1d92347fe7df3667962a542106347b54212b499c
hamzaerengencalioglu <hamzaerengencalioglu@gmail.com>
2023-05-26 22:04:17 +0300

    CryptoApplication updated

--- CryptoApplication/CryptoApp.cs
+++ CryptoApplication/CryptoApp.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoLibrary;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 
 internal class CryptoApp
@@ -7,7 +8,7 @@ internal class CryptoApp
     {
         Console.WriteLine("Crypto Application Running..");
         var cryptoLibrary = new CryptoLibrary.Crypto();
-        string sourceFilePath = "a.txt";
+        string sourceFilePath = "input.txt";
         string sourceText = File.ReadAllText(sourceFilePath);
         
         byte[] data = Encoding.UTF8.GetBytes(sourceText);
@@ -15,45 +16,57 @@ internal class CryptoApp
       
         // SHA-1
         byte[] sha1Hash = Crypto.ComputeSHA1(data);
-        Console.WriteLine("SHA-1: " + cryptoLibrary.ByteArrayToHex(sha1Hash));
+        Console.WriteLine("SHA-1: " + cryptoLibrary.ByteArrayToHex(sha1Hash) + "\n");
 
         // SHA-256
         byte[] sha256Hash = Crypto.ComputeSHA256(data);
-        Console.WriteLine("SHA-256: " + cryptoLibrary.ByteArrayToHex(sha256Hash));
+        Console.WriteLine("SHA-256: " + cryptoLibrary.ByteArrayToHex(sha256Hash)+"\n");
 
         // SHA-512
         byte[] sha512Hash = Crypto.ComputeSHA512(data);
-        Console.WriteLine("SHA-512: " + cryptoLibrary.ByteArrayToHex(sha512Hash));
+        Console.WriteLine("SHA-512: " + cryptoLibrary.ByteArrayToHex(sha512Hash) + "\n");
 
         // DES
         string desKey = "mykey123"; // 8 characters for DES
         byte[] desEncryptedData = cryptoLibrary.DESEncrypt(data, desKey);
         byte[] desDecryptedData = cryptoLibrary.DESDecrypt(desEncryptedData, desKey);
-        Console.WriteLine("DES Decrypted: " + Encoding.UTF8.GetString(desDecryptedData));
+        Console.WriteLine("DES Decrypted: " + Encoding.UTF8.GetString(desDecryptedData) + "\n");
 
         // AES
         string aesKey = "myaeskey12345678"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
         byte[] aesEncryptedData = cryptoLibrary.AESEncrypt(data, aesKey);
         byte[] aesDecryptedData = cryptoLibrary.AESDecrypt(aesEncryptedData, aesKey);
-        Console.WriteLine("AES Decrypted: " + Encoding.UTF8.GetString(aesDecryptedData));
+        Console.WriteLine("AES Decrypted: " + Encoding.UTF8.GetString(aesDecryptedData) + "\n");
 
         // HMAC-SHA1
         byte[] hmacSha1Key = Encoding.UTF8.GetBytes("myhmackey");
         byte[] hmacSha1Hash = cryptoLibrary.ComputeHMACSHA1(data, hmacSha1Key);
-        Console.WriteLine("HMAC-SHA1: " + cryptoLibrary.ByteArrayToHex(hmacSha1Hash));
+        Console.WriteLine("HMAC-SHA1: " + cryptoLibrary.ByteArrayToHex(hmacSha1Hash) + "\n");
 
         // HMAC-SHA256
         byte[] hmacSha256Key = Encoding.UTF8.GetBytes("myhmac256key");
         byte[] hmacSha256Hash = cryptoLibrary.ComputeHMACSHA256(data, hmacSha256Key);
-        Console.WriteLine("HMAC-SHA256: " + cryptoLibrary.ByteArrayToHex(hmacSha256Hash));
+        Console.WriteLine("HMAC-SHA256: " + cryptoLibrary.ByteArrayToHex(hmacSha256Hash) + "\n");
 
         // CRC32
         byte[] crc32Hash = cryptoLibrary.ComputeCRC32(data);
-        Console.WriteLine("CRC32: " + cryptoLibrary.ByteArrayToHex(crc32Hash));
+        Console.WriteLine("CRC32: " + cryptoLibrary.ByteArrayToHex(crc32Hash) + "\n");
 
         // MD5
         byte[] md5Hash = cryptoLibrary.ComputeMD5(data);
-        Console.WriteLine("MD5: " + cryptoLibrary.ByteArrayToHex(md5Hash));
+        Console.WriteLine("MD5: " + cryptoLibrary.ByteArrayToHex(md5Hash) + "\n");
+
+        // CBC
+        string aesKey1 = "myaeskey12345678"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
+
+
+        byte[] encryptedBytes = Crypto.EncryptCBC(data, aesKey1);
+        string decryptedText  = Crypto.DecryptCBC(encryptedBytes, aesKey1);
+
+        Console.WriteLine("Encrypted Text: " + Convert.ToBase64String(encryptedBytes) + "\n");
+        Console.WriteLine("Decrypted Text: " + decryptedText + "\n");
+
+
 
         Console.ReadLine();
     }
