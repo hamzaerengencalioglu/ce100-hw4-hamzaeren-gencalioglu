79c098c0ff9910d8635000c99261fbb15872edad
hamzaerengencalioglu <hamzaerengencalioglu@gmail.com>
2023-05-31 11:52:34 +0300

    CryptoApplication updated

--- CryptoApplication/CryptoApp.cs
+++ CryptoApplication/CryptoApp.cs
@@ -27,19 +27,21 @@ internal class CryptoApp
         Console.WriteLine("SHA-512: " + cryptoLibrary.ByteArrayToHex(sha512Hash) + "\n");
 
         // DES
-        string desKey = "mykey123"; // 8 characters for DES
+        string desKey = "00196761"; // 8 characters for DES
         byte[] desEncryptedData = cryptoLibrary.DESEncrypt(data, desKey);
         byte[] desDecryptedData = cryptoLibrary.DESDecrypt(desEncryptedData, desKey);
+        Console.WriteLine("DES Encrypted: " + Encoding.UTF8.GetString(desEncryptedData)+ "\n");
         Console.WriteLine("DES Decrypted: " + Encoding.UTF8.GetString(desDecryptedData) + "\n");
 
         // AES
-        string aesKey = "myaeskey12345678"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
+        string aesKey = "Trabzonspor01967"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
         byte[] aesEncryptedData = cryptoLibrary.AESEncrypt(data, aesKey);
         byte[] aesDecryptedData = cryptoLibrary.AESDecrypt(aesEncryptedData, aesKey);
+        Console.WriteLine("AES Encrypted: " + Encoding.UTF8.GetString(aesEncryptedData)+ "\n");
         Console.WriteLine("AES Decrypted: " + Encoding.UTF8.GetString(aesDecryptedData) + "\n");
 
         // HMAC-SHA1
-        byte[] hmacSha1Key = Encoding.UTF8.GetBytes("myhmackey");
+        byte[] hmacSha1Key = Encoding.UTF8.GetBytes("Trabzonspor");
         byte[] hmacSha1Hash = cryptoLibrary.ComputeHMACSHA1(data, hmacSha1Key);
         Console.WriteLine("HMAC-SHA1: " + cryptoLibrary.ByteArrayToHex(hmacSha1Hash) + "\n");
 
@@ -57,15 +59,18 @@ internal class CryptoApp
         Console.WriteLine("MD5: " + cryptoLibrary.ByteArrayToHex(md5Hash) + "\n");
 
         // CBC
-        string aesKey1 = "myaeskey12345678"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
-
-
+        string aesKey1 = "aeskey1234567890"; // 16 characters for AES-128, 24 characters for AES-192, 32 characters for AES-256
         byte[] encryptedBytes = Crypto.EncryptCBC(data, aesKey1);
         string decryptedText  = Crypto.DecryptCBC(encryptedBytes, aesKey1);
+        Console.WriteLine("CBC Encrypted Text: " + Convert.ToBase64String(encryptedBytes) + "\n");
+        Console.WriteLine("CBC Decrypted Text: " + decryptedText + "\n");
 
-        Console.WriteLine("Encrypted Text: " + Convert.ToBase64String(encryptedBytes) + "\n");
-        Console.WriteLine("Decrypted Text: " + decryptedText + "\n");
 
+        //HOTP
+        string key = "Trabzonspor";
+        long counter = 123456;
+        string otp = Crypto.GenerateHOTP(key, counter);
+        Console.WriteLine("Generated OTP: " + otp);
 
 
         Console.ReadLine();
